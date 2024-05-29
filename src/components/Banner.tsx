4a50b3268c0c01400b5050c116f723d976625e9e
Yunlong Li <yunlong.li@csiro.au>
2024-05-29 15:14:56 +0800

    add photo rater refer

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,8 +11,8 @@ export default function Banner() {
     return (
         <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
             <p className="text-sm leading-6 text-white">
-                <a href="https://resumego.io" target='_blank'>
-                    <strong className="font-semibold">ResumeGo</strong>
+                <a href="https://photorater.io/" target='_blank' title='photo rater'>
+                    <strong className="font-semibold">Photo Rater</strong>
                     <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
                         <circle cx={1} cy={1} r={1} />
                     </svg>
