b0f47547121d63c23dffb729baef2da93381d6b4
Yunlong Li <yunlong.li@csiro.au>
2024-09-06 19:43:09 +0800

    try to reduce build size

--- components/ButtonSupport.tsx
+++ components/ButtonSupport.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { Crisp } from "crisp-sdk-web";
 import config from "@/config";
 
 // Use this button if chat is hidden on some routes. config.js has onlyShowOnRoutes set to ["/"] so it will be hidden on all routes except the home page.
 // If Crisp is not enable, it will open the support email in the default email client.
 const ButtonSupport = () => {
   const handleClick = () => {
-    if (config.crisp?.id) {
-      Crisp.chat.show();
-      Crisp.chat.open();
-    } else if (config.mailgun?.supportEmail) {
+     if (config.mailgun?.supportEmail) {
       // open default email client in new window with "need help with ${config.appName}" as subject
       window.open(
         `mailto:${config.mailgun.supportEmail}?subject=Need help with ${config.appName}`,
